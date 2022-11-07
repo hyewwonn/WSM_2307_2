f1ef13c5d13ab867dafa4a23d61b3a0b444b3f35
혜원 <w2106@e-mirim.hs.kr>
2022-11-07 11:12:39 +0900

    index refactoring

---
+++ 20220608_school cafeteria/js/nav.js
@@ -0,0 +1,34 @@
+// show/hide menu: toggle menu
+const toggleMenu = (toggleId, navListId) => {
+    //html -> js
+    const toggle = document.getElementById(toggleId);
+    const navList = document.getElementById(navListId);
+    const toggleIcon = toggle.getElementsByTagName("i")[0];
+  
+    if (toggle && navList) {
+      //toggle click
+      toggle.addEventListener("click", () => {
+        //show/hide menu: .show-menu
+        navList.classList.toggle("show-menu");
+        //change toggle icon: bx-menu <-> bx-x
+        toggleIcon.classList.toggle("bx-menu");
+        toggleIcon.classList.toggle("bxs-x-square");
+      });
+    }
+  };
+  toggleMenu("nav-toggle", "nav-list");
+  
+  // const say = () => console.log("hello world4");
+  
+  // const say = () => {
+  //   console.log("hello world3");
+  // };
+  
+  // const say = function () {
+  //   console.log("hello world2");
+  // };
+  
+  // function say() {
+  //   console.log("hello world");
+  // }
+  // say();
